a2491edd164882ff8355799101f6298370f440e4
Matthew Wang <wsh010217@gmail.com>
2024-04-10 18:38:01 -0400

    Revert "change discover page name"
    This reverts commit 1c2dcc685a4c454f5fe89f8d2cb846f8df2d9626.

--- recipeApp/urls.py
+++ recipeApp/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.discover, name="discover"),
+    path("", views.main_site, name="main_site"),
     path("subscriberhome", views.subscriber_home, name="subscriber_home"),
     path("chefhome", views.index, name="index"),
 ]
--- recipeApp/views.py
+++ recipeApp/views.py
@@ -7,13 +7,13 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the recipeApp index.")
 
-def discover(request):
+def main_site(request):
     params = {}   
     if request.user.is_authenticated:
         params['is_authenticated'] = True
         params['avator_dir'] = 'images/sad_cat.jpg'
     
-    return render(request, 'discover.html', params)
+    return render(request, 'mainsite.html', params)
 
 def subscriber_home(request):
     params = {}
