444deea665fb663783436cf960bf639d47c10714
YZCUS <43754173+YZCUS@users.noreply.github.com>
2024-04-16 22:28:31 -0400

    new branch for google login

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'recipe_app',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
