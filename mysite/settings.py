b55da6fd1aa3dd1cea68d6cf4c8d7c9bb5681165
YZCUS <43754173+YZCUS@users.noreply.github.com>
2024-04-24 01:46:14 -0400

    merge main

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'recipe_app',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
