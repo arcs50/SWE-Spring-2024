af4da8db631c3d548da644630b716d94a8c0fa04
YZCUS <43754173+YZCUS@users.noreply.github.com>
2024-04-25 03:39:31 -0400

    update user profile

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'recipe-1',
+        'NAME': 'recipe_app',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
